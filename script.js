07ec2d1c07ee2dea74393de02d07837a0ea61fa0
Sonic <34913159+Sonic322@users.noreply.github.com>
2025-06-17 22:57:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -82,3 +82,39 @@
     console.error('Lucide не загружен');
   }
 });
+
+ // Footer Donate
+  function toggleDonate() {
+    const modal = document.getElementById('donateModal');
+    modal.classList.toggle('hidden');
+  }
+
+  
+  function copyAddress() {
+  const address = document.getElementById("walletAddress").textContent.trim();
+  const btn = document.getElementById("copyBtn");
+
+  navigator.clipboard.writeText(address).then(() => {
+    // Меняем содержимое кнопки на "Copied!" с SVG
+    btn.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16" height="16" viewBox="0 0 16 16" style="margin-right: 6px;">
+<path fill="#bae0bd" d="M7.5 0.5A7 7 0 1 0 7.5 14.5A7 7 0 1 0 7.5 0.5Z"></path><path fill="#5e9c76" d="M7.5,1C11.1,1,14,3.9,14,7.5S11.1,14,7.5,14S1,11.1,1,7.5S3.9,1,7.5,1 M7.5,0C3.4,0,0,3.4,0,7.5 S3.4,15,7.5,15S15,11.6,15,7.5S11.6,0,7.5,0L7.5,0z"></path><path fill="#5e9c76" d="M6.3 10.5L3.7 7.9 4.4 7.2 6.3 9.1 11.2 4.3 11.9 5z"></path>
+</svg>
+  Copied!
+`;
+
+
+    // Через 2 секунды возвращаем оригинальный вид
+    setTimeout(() => {
+      btn.innerHTML = `
+        Copy Address
+      `;
+    }, 2000);
+  });
+}
+
+
+
+
+
+
